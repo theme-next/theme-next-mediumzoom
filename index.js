8fb1aab39b42698390f580a89cac6617933b58cd
StevenJoeZhang <1119186082@qq.com>
2019-10-02 14:31:25 +0800

    Create new tag plugins

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 'use strict';
 
 const utils = require('./utils');
-const fullImage = require('./lib/full-image');
+const includeRaw = require('./lib/include-raw');
+const postVideo = require('./lib/video');
 
-hexo.extend.tag.register('fullimage', fullImage, {ends: false});
-hexo.extend.tag.register('fi', fullImage, {ends: false});
+hexo.extend.tag.register('include_raw', includeRaw, { ends: false, async: true });
+
+hexo.extend.tag.register('video', postVideo, { ends: false });
