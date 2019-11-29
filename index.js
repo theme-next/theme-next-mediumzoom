9a83d1f8ab6473a5ed58cd72d4d18a09a8dab585
Mimi <1119186082@qq.com>
2019-11-29 12:14:24 +0800

    Add linkgrid tag

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 'use strict';
 
-const utils = require('./utils');
+const path = require('path');
 const includeRaw = require('./lib/include-raw');
-const postVideo = require('./lib/video');
+const linkGrid = require('./lib/link-grid');
 
 hexo.extend.tag.register('include_raw', includeRaw, { ends: false, async: true });
 
-hexo.extend.tag.register('video', postVideo, { ends: false });
+hexo.extend.tag.register('linkgrid', linkGrid, { ends: true });
+hexo.extend.tag.register('lg', linkGrid, { ends: true });
+
+hexo.extend.filter.register('theme_inject', function(injects) {
+  injects.style.push(path.join(__dirname, 'link-grid.styl'));
+});
